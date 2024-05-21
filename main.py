1424bb88f412a5a2b21140c41934044edc5cdfb6
Tommaso Travaglia <129117706+TomTrava03@users.noreply.github.com>
2024-05-21 18:13:29 +0200

    Update main.py
    Modified to use .env files

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+#!/usr/bin/env python
+
+from pyVinted import Vinted # modify Vinted library for .it site
+from datetime import datetime
+import time
 import sys
 import pytz
 
 from discord import Webhook
 import aiohttp
 import asyncio
+from dotenv import load_dotenv
 
-# EXAMPLE for vinted.it url: https://www.vinted.it/catalog?order=newest_first&price_to=60&currency=EUR&brand_id[]=1281&catalog[]=19
 
 # GLOBAL VARIABLES #
 vinted = Vinted()
 
 def write_log(clock, message): # Log FILE
+
     try:     
         logs = open("logs.txt", "a")
         log = f"LOG[{clock}]: " + message
@@ -18,13 +24,13 @@ def write_log(clock, message): # Log FILE
     except Exception as e:
         print(f"ERROR on MODIFYING LOG file {clock}]: " + str(e))
 
-async def send_to_discord(clock, message): # Discord WEBHOOK
+async def send_to_discord(clock, message, url_discord_webhook): # Discord WEBHOOK
     # message = f"ITEM: {item.title}\nPRICE: {item.price}€\nURL: {item.url}\n"
     async with aiohttp.ClientSession() as session:
-        webhook = Webhook.from_url(url_discord, session=session)
+        webhook = Webhook.from_url(url_discord_webhook, session=session)
         await webhook.send(message, username='vinted-bot')
 
-def run(url):
+def run(url_vinted, url_discord):
 
     old_items = set()
     first_iteration = True
@@ -33,7 +39,7 @@ def run(url):
         clock = datetime.now(pytz.timezone('Europe/Rome')).strftime("%d/%m/%Y %H:%M:%S")
         
         try:
-            new_items = set(vinted.items.search(url, 10, 1))
+            new_items = set(vinted.items.search(url_vinted, 10, 1))
         except requests.ConnectionError, e:
             print(f"Errore[{clock}]: "e)
 
@@ -42,18 +48,19 @@ def run(url):
             print(element.title)
 
         tmp = new_items.difference(old_items) 
-        old_items = new_items #
+        old_items = new_items 
 
         for element in tmp:
             message = f"ITEM: {element.title}\nPRICE: {element.price}€\nURL: {element.url}\n"
-            # print(message)
-            asyncio.run(send_to_discord(clock, message))
+            asyncio.run(send_to_discord(clock, message, url_discord))
             write_log(clock, message)
         
         time.sleep(300)
 
 def init(): 
-    run(sys.argv[1])
+    load_dotenv()
+    run(sys.argv[1], os.getenv('URL'))
+    
 
 if __name__ == '__main__':
     init()
